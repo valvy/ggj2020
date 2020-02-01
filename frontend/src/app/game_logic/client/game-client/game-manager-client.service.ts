ac80357c64986575f627b7ec504c71a55d5643ce
Mike Hofstede <e.m.j.hofstede@hva.nl>
2020-02-01 11:57:55 +0100

    Add snap to cards, Add help texts for playing cards

--- frontend/src/app/game_logic/client/game-client/game-manager-client.service.ts
+++ frontend/src/app/game_logic/client/game-client/game-manager-client.service.ts
@@ -1,7 +1,7 @@
 import { GameLoaderService } from '../../../services/game-loader.service';
 import { Injectable } from '@angular/core';
 import { FileLoader } from '../../fileloader';
-import { Texture, Sprite, Container, Text, Loader, Point } from 'pixi.js';
+import { Texture, Sprite, Container, Text, Loader, Point, TextStyle } from 'pixi.js';
 import { Card, ActionType, EntityType } from '../../card';
 
 @Injectable({
@@ -15,13 +15,137 @@ export class GameManagerClientService
     private data: any;
     private dragging: boolean;
 
+    private styleTxtDiscard:TextStyle;
+    private styleTxtPlay:TextStyle;
+    private styleTxtTitle:TextStyle;
+    private styleTxtHelp:TextStyle;
+
+
+
     constructor(private gameLoader: GameLoaderService) 
     {
+        this.init();
+    }
 
+    private init(): void{
+        this.styleTxtTitle = new TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 28,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#FFF', '#CCC'], // gradient
+            stroke: '#000',
+            strokeThickness: 5,
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440,
+        });
+        this.styleTxtHelp = new TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 16,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#FFF', '#CCC'], // gradient
+            stroke: '#000',
+            strokeThickness: 5,
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440,
+        });        
+        this.styleTxtDiscard = new TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 28,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#222', '#550000'], // gradient
+            stroke: '#333',
+            strokeThickness: 5,
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440,
+        });
+        this.styleTxtPlay = new TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 28,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#222', '#005500'], // gradient
+            stroke: '#333',
+            strokeThickness: 5,
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440,
+        });        
+
+    }
+    private AddHelpText() : void
+    {
+        const textTitle = new Text('Your hand', this.styleTxtTitle);
+        textTitle.anchor.set(0.5, 0.5);
+        textTitle.x = (window.innerWidth / 2);
+        textTitle.y = 28;
+
+        this.viewport.addChild(textTitle);
+
+        const textHelp = new Text('Play 1 card and Discard 1 card', this.styleTxtHelp);
+        textHelp.anchor.set(0.5, 0.5);
+        textHelp.x = (window.innerWidth / 2);
+        textHelp.y = window.innerHeight - 16;
+
+        this.viewport.addChild(textHelp);
+    }
+
+    private AddCardText(height:any) : void
+    {
+        /* 
+        // TEXT EXAMPLE
+        const style2 = new TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 128,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: 0x00FF00
+        });
+
+        const basicText = new Text('Basic text in pixi', style2);
+        basicText.x = 0;
+        basicText.y = 0;
+
+        this.viewport.addChild(basicText);
+        */
+        
+        const textHelpLeft = new Text('Discard', this.styleTxtDiscard);
+        textHelpLeft.anchor.set(0.5, 0.5);
+        textHelpLeft.x = (window.innerWidth / 4);
+        textHelpLeft.y = height;// - ((window.innerHeight - 100) / 6);         
+        this.viewport.addChild(textHelpLeft);
+
+        const textHelpRight = new Text('Play', this.styleTxtPlay);
+        textHelpRight.anchor.set(0.5, 0.5);
+        textHelpRight.x = (window.innerWidth / 4) * 3;
+        textHelpRight.y = height;// - ((window.innerHeight - 100) / 6);
+        this.viewport.addChild(textHelpRight);
     }
 
     public startGame(): void
     {
+
         this.viewport = this.gameLoader.pixi.stage;
         this.fileLoader = this.gameLoader.fileLoader;
         this.textures = this.fileLoader.getTextures([
@@ -41,15 +165,22 @@ export class GameManagerClientService
 
             const actualHeight = card.actualHeight;
             
-            card.createCard(new Point(window.innerWidth / 2, (actualHeight / 2) + (i * 25) + 25 + i * actualHeight));
+            let positionHeight = (actualHeight / 2) + (i * 25) + 25 + i * actualHeight;
+            card.createCard(new Point(window.innerWidth / 2, positionHeight));
+
+            this.AddCardText(positionHeight);
 
             this.viewport.addChild(card);
         }       
         
+        this.AddHelpText();
+
         this.gameLoader.addGameLoopTicker(this.updateCycle.bind(this));        
     }
 
     private updateCycle(delta: number): void
     {
+
     }
+
 }
