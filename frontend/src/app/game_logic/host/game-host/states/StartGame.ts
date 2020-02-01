c6dc8855a6f19dad89a32ef5793e074bbc47b81d
Jur van Oerle <j.h.van.oerle@hva.nl>
2020-02-01 14:59:31 +0100

    Create game and Wait for Players

---
+++ frontend/src/app/game_logic/host/game-host/states/StartGame.ts
@@ -0,0 +1,14 @@
+import { State } from './State';
+
+export class StartGame extends State
+{
+    public stateStarted(): void
+    {
+
+    }
+
+    public handle(delta: number): void
+    {
+        
+    }
+}
--- frontend/src/app/game_logic/host/game-host/states/WaitingForPlayers.ts
+++ frontend/src/app/game_logic/host/game-host/states/WaitingForPlayers.ts
@@ -1,13 +1,12 @@
-import { Viewport } from 'pixi-viewport';
 import { State } from './State';
 import { TextStyle, Text } from 'pixi.js';
-import { setInterval } from 'timers';
-import { HttpClient } from '@angular/common/http';
+import { StateType } from '../state-manager';
 
 export class WaitingForPlayers extends State
 {
     private _text: Text;
     private _players: number;
+    private _poller;
 
     constructor()
     {
@@ -32,25 +31,43 @@ export class WaitingForPlayers extends State
             wordWrap: true,
             wordWrapWidth: 440,
         });
-        this._text = new Text('Waiting for players...\n' + '', style);
+        this._text = new Text('Creating game...\n' + '', style);
         this._text.anchor.set(0.5, 0.5);
         this._text.x = window.innerWidth / 2;
         this._text.y = window.innerHeight / 2;
         this._viewport.addChild(this._text);
-        setInterval(() => this.pollForPlayers(), 1500);
+        this.generateWorld();
+    }
+
+    private generateWorld(): void
+    {
+        this._stateManager.doGetRequest('http://localhost:9000/game/generate').subscribe((data) =>
+        {
+            if (data['Hello world'])
+            {
+                this._text.text = 'Waiting for players...';
+                this._poller = setInterval(() => this.pollForPlayers(), 1500);
+            }            
+        });
     }
 
     private pollForPlayers(): void
     {
-        this._stateManager.doGetRequest().subscribe((data) =>
+        this._stateManager.doGetRequest('http://localhost:9000/game/player/count').subscribe((data) =>
         {
-            
+            const numberofplayers: number = data.Online;
+            this._text.text = 'Waiting for players...\n' + numberofplayers + ' players connected.';
+            if (numberofplayers === 4)
+            {
+                this._stateManager.gotoState(StateType.WaitingForInput);
+                clearInterval(this._poller);
+            }
         });
     }
 
     public stateEnded(): void
     {
-        
+        this._viewport.removeChild(this._text);
     }
 
     public handle(delta: number): void
